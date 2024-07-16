f1c73043cb39fc7696ea97f6d5c48a69d7c5d768
unknown <peterskelvin123@gmail.com>
2024-07-15 19:14:02 -0700

    modal component for all form of modal

---
+++ src/components/allmodal.js
@@ -0,0 +1,45 @@
+import { Children } from "react";
+import { Modal, Button } from "react-bootstrap";
+export const Mainmodal = (props) => {
+  const children = Children.toArray(props.children);
+  return (
+    <Modal
+      show={props.showModal}
+      onHide={() => props.actions.control(props.ctrl,"close")}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <center>
+            <h3>{props.title}</h3>
+          </center>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {children.map((child, index) => (
+          <span key={index}>{child}</span>
+        ))}
+      </Modal.Body>
+      <Modal.Footer style={{marginTop:"59px"}}>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            props.actions.control(props.ctrl,"close");
+          }}
+        >
+          {props.footer.close}
+        </Button>
+        {props.actions.mainfunction && (
+          <Button
+            variant="primary"
+            onClick={() => {
+              props.actions.mainfunction();
+              props.actions.control(props.ctrl,"close");
+            }}
+          >
+            {props.footer.mainfunction}
+          </Button>
+        )}
+      </Modal.Footer>
+    </Modal>
+  );
+};
